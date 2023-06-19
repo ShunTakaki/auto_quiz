88ac76b69a0d7de3e768bda0c333b7e793910164
s-takaki22 <s.takaki.sys22@morijyobi.ac.jp>
2023-06-19 10:32:35 +0900

    クイズ機能完成

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
-from flask import Flask, render_template, request
-from common.models.user import db
-from __init__ import create_app
+from flask import Flask, render_template, request, redirect, session
+from controllers.quiz import generate_quiz
+from difflib import SequenceMatcher
 
-app, db = create_app()
+app = Flask(__name__)
+app.secret_key = 'Your Secret Key'
 
 @app.route('/')
 def index():
-    title = 'index'
-    return render_template('index.html', title=title)
+    return render_template('index.html')
 
+@app.route('/quiz', methods=['POST'])
+def quiz():
+    theme = request.form['theme']
+    question, answer = generate_quiz(theme)
+    print('checkpoint1')
+    session['answer'] = answer
+    return render_template('quiz.html', question=question)
+
+@app.route('/check_answer', methods=['POST'])
+def check_answer():
+    user_answer = request.form['user_answer']
+    nowanswer = session.get('answer')
+    print(f'checkpoint2{nowanswer}')
+
+    similarity = SequenceMatcher(None, user_answer.lower(), nowanswer.lower()).ratio()
+    print(f"ユーザーの答え：{user_answer}")
+    print(f"模範解答：{nowanswer}")
+    print(f"一致度：{similarity}")
+
+    if similarity >= 0.5:
+        result = "正解です！"
+        print('checkpoint3')
+    elif similarity >= 0.1:
+        result='惜しいです...なお、解答に不備があった場合には申し訳ございません。'
+        print('checkpoint4')
+    else:
+        result = "間違いです。"
+        print('checkpoint5')
+    return render_template('result.html', result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ controllers/quiz.py
@@ -0,0 +1,24 @@
+import openai
+
+openai.api_key = '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'
+
+def generate_quiz(theme):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "あなたは助けになるアシスタントです。"},
+            {"role": "user", "content": f"{theme}についてのi一問一答のような簡単なクイズを一つだけ作成してください。回答はかならず、 '問題: ... 答え: ...'という形式で出力してください"},
+        ],
+        max_tokens=70
+    )
+
+    print('checkpoint5')
+    assistant_reply = response['choices'][0]['message']['content']
+
+    if "答え:" in assistant_reply:
+        print('checkpoint6')
+        question, answer = assistant_reply.split("答え:", 1)
+        question = question.replace("問題:", "").strip()
+        return question, answer.strip()
+    else:
+        return "予期しない形式の応答が返されました", ""
